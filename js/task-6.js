96f4d62e651b2325965d55484ec04e66edbf490f
Roman Dobrodii <64207444+Roman-Dobrodii@users.noreply.github.com>
2020-08-22 18:21:17 +0300

    Добавляет решение задачи №6

--- js/task-6.js
+++ js/task-6.js
@@ -26,8 +26,11 @@
 function mapArray(array) {
   'use strict';
   const numbers = new Array(array.length);
+
   for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
+    numbers[i] = array[i] * 10;
+    // console.log(array[i] * 10);
   }
   return numbers;
 }
